d6c8c57082af5189fee917e68a8d1450433c71cb
valm-software <143118398+valm-software@users.noreply.github.com>
2023-10-23 10:03:08 +0200

    Update run.py

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5002)
+    app.run(host='0.0.0.0',port=8000,ssl_context=('certificate.crt', 'private.key'))
